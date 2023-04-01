809a1df393103a180612860a0d04430506d8655d
encarnacao <cencarnacaoq@gmail.com>
2023-04-01 17:04:04 -0300

    feat: use router and error handling

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
+import "express-async-errors";
 import express, { json } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import routes from "./routers/index.js";
+import { handleApplicationErrors } from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
+app.use(routes);
+app.use(handleApplicationErrors);
 
 const PORT = process.env.PORT || 5000;
 
