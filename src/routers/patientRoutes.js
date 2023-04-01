efa40b9075f1b6432ba8a5e1ed9122c15954131d
encarnacao <cencarnacaoq@gmail.com>
2023-04-01 19:09:10 -0300

    feat: add sign in route for patients

--- src/routers/patientRoutes.js
+++ src/routers/patientRoutes.js
@@ -1,10 +1,19 @@
-import { Router } from 'express';
-import { validateSchema } from '../middlewares/schemaValidation.js';
-import patientSchema from '../schemas/patientSchema.js';
-import patientControllers from '../controllers/patientControllers.js';
+import { Router } from "express";
+import { validateSchema } from "../middlewares/schemaValidation.js";
+import patientSchema from "../schemas/patientSchema.js";
+import patientControllers from "../controllers/patientControllers.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import authController from "../controllers/authController.js";
 
 const patientRouter = Router();
 
-patientRouter.post("/signup", validateSchema(patientSchema),patientControllers.create);
+patientRouter.post(
+	"/signup",
+	validateSchema(patientSchema),
+	authMiddleware.checkConflicts("patients"),
+	patientControllers.create
+);
+
+patientRouter.post("/signin", authController.signin("patients"));
 
 export default patientRouter;
