00ede51e2d3a373d10f5ac23bf470e59921a3225
encarnacao <cencarnacaoq@gmail.com>
2023-04-01 19:02:58 -0300

    feat: add conflict and credential checks

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,36 @@
+import errors from "../errors/index.js";
+import authRepository from "../repositories/authRepository.js";
+import jwt from "jsonwebtoken";
+
+function checkConflicts(table) {
+	return async (req, _, next) => {
+		const { email } = req.body;
+		try {
+			const user = await authRepository.searchEmail(email, table);
+			if (user.length > 0) {
+				throw errors.conflictError(email);
+			}
+		} catch (e) {
+			next(e);
+		}
+		next();
+	};
+}
+
+function validateCredentials(table) {
+	return async (req, res, next) => {
+		const token = req.headers.authorization?.replace("Bearer ", "");
+		if (!token) throw errors.unauthorizedError();
+		try {
+			const { email } = jwt.verify(token, process.env.JWT_SECRET);
+			const user = await authRepository.searchEmail(email, table);
+			if (user.length === 0) throw errors.unauthorizedError();
+			res.locals.user = user[0];
+		} catch (e) {
+			next(e);
+		}
+		next();
+	};
+}
+
+export default { checkConflicts, validateCredentials };
