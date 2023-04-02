7d9650ac00f479c853ee25c1e19f6f773cda1b37
encarnacao <cencarnacaoq@gmail.com>
2023-04-02 01:24:15 -0300

    fix: change doctor schema to match database

--- src/schemas/doctorSchema.js
+++ src/schemas/doctorSchema.js
@@ -4,10 +4,10 @@ const doctorSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    registration: Joi.string().max(6).required(),
+    registration: Joi.string().length(6).required(),
     specialty: Joi.string().required(),
     city: Joi.string().required(),
-    state: Joi.string().required(),
+    state: Joi.string().length(2).required(),
 });
 
 
