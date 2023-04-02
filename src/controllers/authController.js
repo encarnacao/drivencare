d7c1d1df243470a71c66cf198444c716c246f486
encarnacao <cencarnacaoq@gmail.com>
2023-04-02 10:00:45 -0300

    refactor: improve readability

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,18 +1,18 @@
 import errors from "../errors/index.js";
 import authServices from "../services/authServices.js";
 
-
-function signin(table){
-    return async (req, res, next) => {
-        const { email, password } = req.body;
-        if (!email || !password) throw errors.badRequestError("Email and password are required");
-        try {
-            const token = await authServices.signin(email, password, table);
-            res.status(200).send({ token });
-        } catch (e) {
-             next(e);
-        }
-    };
+function signin(table) {
+	return async (req, res, next) => {
+		const { email, password } = req.body;
+		if (!email || !password)
+			throw errors.badRequestError("Email and password are required");
+		try {
+			const token = await authServices.signin(email, password, table);
+			res.status(200).send({ token });
+		} catch (e) {
+			next(e);
+		}
+	};
 }
 
 export default { signin };
