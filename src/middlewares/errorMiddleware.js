240c004242f539feceb809edcc78263aaa1534b3
encarnacao <cencarnacaoq@gmail.com>
2023-04-02 10:41:54 -0300

    refactor: generalize error middelware

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,55 +1,10 @@
 export function handleApplicationErrors(err, req, res, next) {
-	if (err.name === "EmailConflictError") {
-		return res
-			.status(err.status)
-			.send({ message: err.message, email: err.email });
-	}
-
-	if (err.name === "InvalidCredentialsError") {
-		return res.status(err.status).send({
-			message: err.message,
-		});
-	}
-
-	if (err.name === "TimeConflictError") {
-		return res.status(err.status).send({
-			message: err.message,
-			date: err.date,
-			time: err.time,
-		});
-	}
-
-	if (err.name === "UnprocessableEntityError") {
-		return res.status(err.status).send({
-			message: err.message,
-		});
-	}
-
-	if (err.name === "UnauthorizedError") {
-		return res.status(err.status).send({
-			message: err.message,
-		});
-	}
-
-	if (err.name === "NotFoundError") {
-		return res.status(err.status).send({
-			message: err.message,
-		});
-	}
-
-	if (err.name === "BadRequestError") {
-		return res.status(err.status).send({
-			message: err.message,
-		});
-	}
-
-	if (err.name === "ForbiddenError") {
-		return res.status(err.status).send({
-			message: err.message,
-		});
-	}
-
 	console.log(err);
+	if (err.name) {
+		const error = {	...err }
+		delete error.status;
+		return res.status(err.status).send(error);
+	}
 	return res.status(500).send({
 		error: "Internal Server Error",
 		message: err,
