f9beb0f07a5404cf545d48fd1754cca7c3c1f310
encarnacao <cencarnacaoq@gmail.com>
2023-04-02 01:21:34 -0300

    fix: e-mail and password check must be done separately

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,8 +11,9 @@ function signin(table){
         if (!email || !password) throw errors.badRequestError("Email and password are required");
         try {
             const user = await authRepository.searchEmail(email, table);
-            const isPasswordCorrect = await bcrypt.compare(password, user[0]?.password);
-            if(!isPasswordCorrect || user.length === 0) throw errors.invalidCredentialsError();
+            if( user.length === 0) throw errors.invalidCredentialsError();
+            const isPasswordCorrect = await bcrypt.compare(password, user[0].password);
+            if(!isPasswordCorrect) throw errors.invalidCredentialsError();
             const token = jwt.sign({ email: user[0].email }, process.env.JWT_SECRET);
             res.status(200).send({ token });
         } catch (e) {
