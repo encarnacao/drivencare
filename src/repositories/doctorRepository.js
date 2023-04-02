caf003af81903913167ed0575bd178543c407b5a
encarnacao <cencarnacaoq@gmail.com>
2023-04-02 01:23:49 -0300

    feat: add doctor repository to handle creation

---
+++ src/repositories/doctorRepository.js
@@ -0,0 +1,22 @@
+import database from "../config/database.js";
+
+async function createDoctor(
+	name,
+	email,
+	password,
+	specialty,
+	city,
+	state,
+	registration
+) {
+	const { rows } = await database.query(
+		`INSERT INTO doctors (name, email, password, specialty, city, state, registration) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7) 
+        RETURNING id, name, email, specialty, city, state, registration, created_at`,
+		[name, email, password, specialty, city, state, registration]
+	);
+	return rows;
+}
+
+
+export default { createDoctor };
