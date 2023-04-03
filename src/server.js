8603421d437f6aedde66b1673c45abff957d937e
encarnacao <cencarnacaoq@gmail.com>
2023-04-03 15:52:41 -0300

    feat: use set interval to send e-mails to appointments every day

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import routes from "./routers/index.js";
 import { handleApplicationErrors } from "./middlewares/errorMiddleware.js";
+import sendEmails from "./scripts/emailSender.js";
 
 dotenv.config();
 
@@ -17,4 +18,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
+	setInterval(sendEmails, 1000 * 60 * 60 * 24);
 });
